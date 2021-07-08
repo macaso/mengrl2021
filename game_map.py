f978b18702865bfa2dfdf34ff62f99d0dd4c89c8
Marc Casas <marc@ecmos.com>
2021-07-08 22:48:03 +0200

    Add bitmask rendering

--- game_map.py
+++ game_map.py
@@ -1,5 +1,6 @@
 import numpy as np
 from tcod.console import Console
+from tcod.tileset import CHARMAP_CP437
 
 import tile_types
 
@@ -12,5 +13,54 @@ class GameMap:
         """ Return True if x and y are inside of the bounds of this map. """
         return 0 <= x < self.width and 0 <= y < self.height
 
+    # Really unoptimized bitmask renderer based on the RLTK Tutorial
     def render(self, console: Console) -> None:
-        console.tiles_rgb[0: self.width, 0: self.height] = self.tiles["dark"]
+        # The original, FAST, NumPy renderer
+        # console.tiles_rgb[0: self.width, 0: self.height] = self.tiles["dark"]
+
+        # The new, SLOW, bitmask renderer
+        for x in range(0, self.width):
+            for y in range(0, self.height):
+                tile = self.tiles[x, y]["dark"]
+                if self.tiles[x, y] == tile_types.wall:
+                    tile[0] = self.wall_glyph(x, y)
+
+                console.tiles_rgb[x, y] = tile
+
+    def is_revealed_and_wall(self, x: int, y: int) -> bool:
+        return self.tiles[x, y] == tile_types.wall
+
+    def wall_glyph(self, x: int, y: int) -> int:
+        if x < 1 or x > self.width - 2 or y < 1 or y > self.height - 2:
+            return 35
+
+        mask = 0
+        if self.is_revealed_and_wall(x, y - 1):
+            mask += 1
+        if self.is_revealed_and_wall(x, y + 1):
+            mask += 2
+        if self.is_revealed_and_wall(x - 1, y):
+            mask += 4
+        if self.is_revealed_and_wall(x + 1, y):
+            mask += 8
+
+        tiles = {
+            0: 9,       # No neighbors
+            1: 186,     # Wall N
+            2: 186,     # Wall S
+            3: 186,     # Wall NS
+            4: 205,     # Wall W
+            5: 188,     # Wall NW
+            6: 187,     # Wall SW
+            7: 185,     # Wall NSW
+            8: 205,     # Wall E
+            9: 200,     # Wall NE
+            10: 201,    # Wall SE
+            11: 204,    # Wall NSE
+            12: 205,    # Wall EW
+            13: 202,    # Wall SWE
+            14: 203,    # Wall NWE
+            15: 206     # Wall on all sides
+        }
+
+        return CHARMAP_CP437[tiles.get(mask, 35)]

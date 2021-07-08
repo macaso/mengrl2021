1921c35c31b1c7db9f77433b271c31ee7f1644d7
Marc Casas <marc@ecmos.com>
2021-07-08 06:56:38 +0200

    Finished part 2

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
 import tcod
 
-from actions import EscapeAction, MovementAction
+from engine import Engine
+from entity import Entity
+from game_map import GameMap
 from input_handlers import EventHandler
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
 
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+    map_width = 80
+    map_height = 45
 
     tileset = tcod.tileset.load_tilesheet(
         "images/Redjack17.png", 16, 16, tcod.tileset.CHARMAP_CP437
     )
 
     event_handler = EventHandler()
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2) - 5, int(screen_height / 2), "@", (255, 255, 0))
+    entities = {npc, player}
+
+    game_map = GameMap(map_width, map_height)
+    engine = Engine(entities = entities, event_handler = event_handler, game_map = game_map, player = player)
 
     with tcod.context.new_terminal(
         screen_width,
@@ -26,22 +35,11 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order = "F")
         while True:
-            root_console.print(x = player_x, y = player_y, string = "@")
-            context.present(root_console)
-            root_console.clear()
+            engine.render(console = root_console, context = context)
 
-            for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
+            events = tcod.event.wait()
 
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):
-                    player_x += action.dx
-                    player_y += action.dy
-
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
+            engine.handle_events(events)
 
 if __name__ == "__main__":
     main()
